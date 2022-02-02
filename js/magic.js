e0b633dbf40d256fb272dfdc4b17815f6db5905e
MAStanciu <mihaiaurelianstanciu@campuscamara.es>
2022-02-02 14:12:01 +0100

    form commit

--- js/magic.js
+++ js/magic.js
@@ -1,9 +1,24 @@
 function myFirstFunction(){
     console.log("Thanks you for click");
+    
+    var div = document.getElementById("my_div");
+    div.classList.add("my_class");
+    console.log(div);
+
+    var div = document.getElementsByTagName("div");
+    console.log(div);
+
+    var second_div = document.querySelector("#my_second_div");
+    console.log(second_div);
+
+    console.log($("#my_third_div"));
 }
+
 function mySecondFunction(){
     console.log("Holaaa");
 }
 
-var div = document.getElementsByClassName("div_magic");
-console.log(div);
+function $(selector){
+    return document.querySelectorAll(selector);
+
+}
